b1f1e1e087ac2c45cc4245992d269e2553cf4fc9
ViriatoII <ricardoguerreiro21@hotmail.com>
2022-01-28 15:52:48 +0100

    Includes roi reading and plotting

--- polylux_python.py
+++ polylux_python.py
@@ -1,5 +1,5 @@
 
-''' ATTENTION: There is a bug related to changing point sizes and colors that can only be fixed by Napari itself.
+''' ATTENTION: There is a bug related to changing point sizes and colors that can only be fixed by the Napari creators.
 
  If you want to change the size or color of the points of a gene, you have to:
     - Select the gene in the lower left pannel
@@ -9,7 +9,6 @@
     - Click on the zoom icon (magnifier glass) to be able to navigate again.
 
 ### INSTALLATION:
- 
  You need Python installed. Run the following in your command prompt:
 
  Optional:
@@ -18,17 +17,13 @@
 
  pip install "napari[all]"
  pip install "napari[all]" --upgrade
-
  pip install pandas 
-
+ 
 More information here: https://napari.org/tutorials/fundamentals/installation.html
 
 ### RUNNING:
-
 1) Edit INPUTS AREA with information on the input files and genes of interest
-
 2) Enter a command prompt and change directory (cd) to where the script is located
-
 3) Run the script: python polylux_python.py
 
 
@@ -42,6 +37,7 @@ from matplotlib import image
 import pandas as pd
 import random 
 import napari
+import roifile
 from matplotlib import cm
 import numpy as np
 
@@ -50,7 +46,7 @@ import numpy as np
 # Files to read
 JPEG_file   = "../08_sample_comparinson/Panorama_32542-slide5_A2_5_Channel3_R8_.jpg"
 coords_file = "../08_sample_comparinson/Panorama_32542-slide5_A2_5_results.txt"
-
+rois = "../08_sample_comparinson/rois/RoiSet_Slide5_A2.zip"
 
 # Genes to plot  (First ones go to bottom)
 genes = ['Adgre1', 'Cd5l', 'Gpnmb',  
@@ -60,8 +56,7 @@ genes = ['Adgre1', 'Cd5l', 'Gpnmb',
  'Sox9']                
 
 # Change this if you'd like all points to be smaller or bigger
- point_size = 20              
-
+point_size = 20              
 
 ######## Preprocessing ###########################
 
@@ -69,13 +64,17 @@ img = image.imread(JPEG_file).T
 df = pd.read_csv(coords_file, sep = '\t')
 df.columns = ['x','y','z','name','qual']
 
-
 # Colours for each gene
 colours = cm.rainbow(np.linspace(0, 1, len(genes)))
+
+# Prepare Rois for plotting
+cell_rois = roifile.roiread(rois)
+polygons = [roi.coordinates() for roi in cell_rois]
 
 
 ########### NAPARI APP ######################################
 
+#Used for the scale
 nanometers_per_pixel = [138, 138]
 
 # Start Napari  
@@ -84,6 +83,11 @@ viewer = napari.view_image(img, scale = nanometers_per_pixel)
 viewer.scale_bar.unit = "nm"
 viewer.scale_bar.events.visible 
 
+
+# Add rois to image
+viewer.add_shapes(polygons, name = "Rois", scale = nanometers_per_pixel,
+                  shape_type='polygon', edge_width=4, 
+                  edge_color='red')
 
 # Add points to image
 for i,gen in enumerate(genes):
@@ -94,7 +98,6 @@ for i,gen in enumerate(genes):
 
     viewer.add_points(points, name = gen, edge_width = 0.01, scale = nanometers_per_pixel,
         face_color= colours[i], size = point_size)                    # experimental_canvas_size_limits = (30,10000))
-
 
 napari.run()  # start the "event loop" that maintains window open
 
